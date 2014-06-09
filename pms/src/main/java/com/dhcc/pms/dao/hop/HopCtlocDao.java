07d309ddc71eed149b9dcb408b10b10379333fb1
Administrator <Administrator@2013-20131023FV>
2014-06-09 15:20:52 +0800

    bug修复

--- pms/src/main/java/com/dhcc/pms/dao/hop/HopCtlocDao.java
+++ pms/src/main/java/com/dhcc/pms/dao/hop/HopCtlocDao.java
@@ -154,6 +154,7 @@ public class HopCtlocDao extends HibernatePersistentObjectDAO<HopCtloc> {
 		
 		pagerModel.setCountProName("CTLOC_ID");
 		pagerModel.setQueryHql(hqlBuffer.toString());
+		pagerModel.setHqlParamMap(hqlParamMap);
 		int totalRows = pagerModel.getTotals();
 		if (totalRows == 0) {
 			totalRows = commonDao.getResultCountWithValuesMap(
